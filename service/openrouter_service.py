613dca368cc537cbdb3187f3eaa77ecef0f718d6
Pedro <pedro.link25@gmail.com>
2026-03-29 13:34:15 -0300

    FEAT: Configs iniciais

--- service/openrouter_service.py
+++ service/openrouter_service.py
@@ -1,6 +1,4 @@
 import requests
-import json
-
 import os
 
 
@@ -8,7 +6,7 @@ def open_router(prompt):
     response = requests.post(
         url="https://openrouter.ai/api/v1/chat/completions",
         headers={
-            "Authorization": f"Bearer "
+            "Authorization": f"Bearer {os.environ.get('API_KEY')}"
         },
         json={
             "model": "openai/gpt-4o-mini",
